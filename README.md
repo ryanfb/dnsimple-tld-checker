ddb643ed9d866182140e0a13c5e6c6fec6c05783
Ryan Baumann <ryan.baumann@gmail.com>
2023-02-23 09:43:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# dnsimple-tld-checker
+
+Ruby script(s) for checking all available [DNSimple TLDs](https://dnsimple.com/tlds) for domain availability, alongside their prices.
+
+Need a domain registrar? Not yet signed up for DNSimple? Use my referral link to sign up here: <https://dnsimple.com/r/a18daa6869b815>
+
+You'll need to set your DNSimple account ID and API access token in the environment variables `DNSIMPLE_ACCOUNT_ID` and `DNSIMPLE_ACCESS_TOKEN` respectively. You can also set these in a `.env` file in the working directory. You can learn how to generate an access token here: <https://support.dnsimple.com/articles/api-access-token/>
+
+Usage:
+
+    bundle exec ./dnsimple-tld-checker.rb myname myproduct myidea
+
+Standard output will be e.g.:
+
+```
+domain  registration    renewal
+myname.academy  $35.00  $35.00
+myname.accountant       $144.00 $144.00
+myname.accountants      $105.00 $105.00
+myname.actor    $44.00  $44.00
+myname.adult    $110.00 $110.00
+myname.aero     $80.00  $80.00
+...
+```
+
+The output is tab-separated and suitable for piping to a .tsv file.
+
+The script uses a simple WHOIS check and heuristic for quickly checking availability. No available libraries I found were suitable for the variety of TLDs supported by DNSimple, and the DNSimple checkDomain API endpoint has a very restrictive rate limit.
---
+++ dnsimple-tld-checker.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+require 'dnsimple'
+require 'dotenv/load'
+require 'money'
+
+Money.rounding_mode = BigDecimal::ROUND_HALF_EVEN
+Money.locale_backend = nil
+
+# Obtain your API token and Account ID
+# https://support.dnsimple.com/articles/api-access-token/
+dnsimple = Dnsimple::Client.new(
+  access_token: ENV['DNSIMPLE_ACCESS_TOKEN']
+)
+
+ACCOUNT_ID = ENV['DNSIMPLE_ACCOUNT_ID']
+
+dnsimple_tlds = dnsimple.tlds.all_tlds
+tld_suffixes = dnsimple_tlds.data.reject{|t| t.tld_type == 2}.map{|t| t.tld}.reject{|t| t.include?('.')}
+
+puts "domain\tregistration\trenewal"
+ARGV.each do |domain_base|
+  tld_suffixes.each do |tld_suffix|
+    domain_to_check = "#{domain_base}.#{tld_suffix}"
+    domain_available = false
+    whois_output = `whois #{domain_to_check} 2>/dev/null | grep -i '^domain status'`
+    domain_available = !whois_output.include?('Prohibited') 
+    if domain_available
+      price_check_response = dnsimple.registrar.get_domain_prices(ACCOUNT_ID, domain_to_check)
+      puts "#{domain_to_check}\t#{Money.from_cents((price_check_response.data.registration_price.to_f * 100.0).to_i, "USD").format}\t#{Money.from_cents((price_check_response.data.registration_price.to_f * 100.0).to_i, "USD").format}"
+    end
+  rescue Dnsimple::RequestError => e
+    if e.message.include?('is not supported')
+      warn "Error checking #{domain_to_check}: #{e.inspect}"
+    else
+      reset_at = e.http_response.headers['x-ratelimit-reset'].to_i
+      reset_in = reset_at - Time.now.to_i
+      warn "Error checking #{domain_to_check}: #{e.inspect}, sleeping #{reset_in} second(s) before retrying (next retry at: #{Time.at(reset_at)})"
+      sleep reset_in
+      retry
+    end
+  end
+end
